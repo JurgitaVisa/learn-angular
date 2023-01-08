9223db10a716e371c9244922e034f4f570e877d7
JurgitaVisa <jurgita.visakaviciute@gmail.com>
2023-01-08 14:41:41 +0200

    lesson 4 bootstrap, style/class/patameter binding, inputs, pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './components/course/course.component';
 import { CoursesService } from './courses.service';
 import { AuthorsComponent } from './components/authors/authors.component';
+import { SummaryPipe } from './summary.pipe';
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
     CourseComponent,
-    AuthorsComponent
+    AuthorsComponent,
+    SummaryPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [
     CoursesService
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -7,15 +7,70 @@ import { CoursesService } from './courses.service';
     <h2>{{title}}</h2>
     <ul>
         <li *ngFor="let course of courses">{{course}}</li>
-    </ul>   
+    </ul>  
+    <p>{{course.title | uppercase}}</p>
+    <p>{{course.students | number}}</p>
+    <p>{{course.rating | number:'1.2-2'}}</p>
+    <p>{{course.rating | number:'2.1-1'}}</p>
+    <p>{{course.price | currency:'EUR':true:'2.2-2'}}</p>
+    <p>{{course.releaseDate | date:'yyyy-MM-dd'}}</p>
+    <br>
+    <p>{{text | summary:10 }}</p>
+
+    <input type="text" class="form-control w-50 my-2" (keyup.enter)="onKeyUp($event)">  
+    <input type="text" class="form-control w-50 my-2" #name (keyup.enter)="onEnter(name.value) ">  
+    <input type="text" class="form-control w-50 my-2" [(ngModel)]="surname" (keyup.enter)="onEnterField()">  
+    <div (click)="onDivClick($event)">
+        <button class="btn btn-outline-primary" 
+         [class.active-test]="isActive"
+         [style.width]="isActive? '250px':'150px'"
+         (click)="onSave($event)"
+        >
+         Save
+        </button>  
+    </div> 
     `
 })
 export class CoursesComponent {
     title = "List of courses";
     courses;
+    isActive = true;
+    surname: string = '';
+    course = {
+        title: "Angular for beginers",
+        rating: 4.92158,
+        students: 30123,
+        price: 12.5,
+        releaseDate: new Date(2022, 9, 24) //prideda 1 men; formatai https://angular.io/api/common/DatePipe
+
+    }
+    text = `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos rerum deserunt quod ducimus. Velit, assumenda corporis veniam, officiis maxime ipsam iusto, quam esse rerum accusantium architecto unde ea porro quis.`
 
     constructor(service: CoursesService) {
         this.courses = service.getCourses();
+    }
+
+    onSave($event: any) {
+        // $event.stopPropagation(); //to stop event bubble up
+        console.log("button clicked: ", $event);
+    }
+
+    onDivClick($event: any) {
+        console.log("Div clicked: ", $event);
+    }
+
+    onKeyUp($event: any) {
+        console.log("Enter pressed: ", $event.target.value);
+        $event.target.value = '';
+    }
+
+    onEnter(name: string) {
+        console.log("Name input: ", name);
+    }
+
+    onEnterField() {
+        console.log("Enter surname: ", this.surname);
+        this.surname = '';
     }
 
 }
